517f4d5a2fe75c09dde316a21a2124bda421c473
BabyDrill <96079788+BabyDrill@users.noreply.github.com>
2023-02-20 19:52:12 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes'
+
+author 'BabyDrill'
+
+name 'wolf_jobs'
+
+version '1.0'
+
+description 'The best FREE Fivem Job Center https://discord.gg/yjPGt2YMcg'
+
+ui_page 'html/wolf.html'
+
+client_scripts {
+    "config/client_config.lua",
+    "client/*.lua"
+} 
+
+server_scripts {
+    "config/*.lua",
+    "server/*.lua"
+}
+
+escrow_ignore {
+    'config/*.lua'
+}
+
+files {
+	'html/wolf.html',
+    'html/wolf.js'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/props/frutta-verdura/knjgh_fruits.ytyp'
---
+++ item.sql
@@ -0,0 +1,11 @@
+INSERT INTO `items` (`name`, `label`) VALUES
+('legnia', 'Legnia'),
+('legnia2', 'Legnia Processata'),
+('arancia', 'Arancia'),
+('gallina', 'Gallina'),
+('gallina2', 'Gallina Imballata'),
+('cannapesca', 'Canna da Pesca'),
+('pesce_spada', 'Pesce Spada'),
+('merluzzo', 'Merluzzo'),
+('tonno', 'Tonno'),
+('sardina', 'Sardina');
